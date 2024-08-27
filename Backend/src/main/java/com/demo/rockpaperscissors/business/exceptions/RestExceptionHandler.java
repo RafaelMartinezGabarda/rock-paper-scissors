bc61d43df577a1fd3b3f8ed5436eed58fc696103
Rafael Martinez Gabarda <rafael.martinez.gabarda@gmail.com>
2024-08-27 17:02:06 +0200

    Logging

--- Backend/src/main/java/com/demo/rockpaperscissors/business/exceptions/RestExceptionHandler.java
+++ Backend/src/main/java/com/demo/rockpaperscissors/business/exceptions/RestExceptionHandler.java
@@ -1,22 +1,28 @@
 package com.demo.rockpaperscissors.business.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    private static Logger log = LoggerFactory.getLogger(RestExceptionHandler.class);
 
     @ExceptionHandler(GameNotFoundException.class)
     public ResponseEntity<Object> handleGameNotFoundException(GameNotFoundException ex, final WebRequest request) {
+        log.error(GameNotFoundException.class.getName(), ex);
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
     @ExceptionHandler(OptionNotFoundException.class)
     public ResponseEntity<Object> handleOptionNotFoundException(OptionNotFoundException ex, final WebRequest request) {
+        log.error(OptionNotFoundException.class.getName(), ex);
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
