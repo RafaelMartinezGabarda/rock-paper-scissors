eca8cfc94571ffd265e5f980b3d8ebfe18ef68b1
Rafael Martinez Gabarda <rafael.martinez.gabarda@gmail.com>
2024-09-04 01:23:17 +0200

    Reorganize components

--- Client/src/app/features/game/game.service.ts
+++ Client/src/app/features/game/game.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { GameResult } from './models/game';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +12,8 @@ export class GameService {
 
   constructor(private httpClient: HttpClient) {}
 
-  createNewGame(): Observable<any> {
-    return this.httpClient.post(this.baseUrl, null).pipe(
+  createNewGame(): Observable<GameResult> {
+    return this.httpClient.post<GameResult>(this.baseUrl, null).pipe(
       catchError((error) => {
         // Handle the error, I keep it empty for the showcase
         throw error;
@@ -20,17 +21,19 @@ export class GameService {
     );
   }
 
-  playRound(id: number, option: string): Observable<any> {
-    return this.httpClient.put(`${this.baseUrl}/play/${id}`, option).pipe(
-      catchError((error) => {
-        alert('Could not make move');
-        throw error;
-      })
-    );
+  playRound(id: number, option: string): Observable<GameResult> {
+    return this.httpClient
+      .put<GameResult>(`${this.baseUrl}/play/${id}`, option)
+      .pipe(
+        catchError((error) => {
+          alert('Could not make move');
+          throw error;
+        })
+      );
   }
 
-  getOptions(): Observable<any> {
-    return this.httpClient.get(`${this.baseUrl}/options`).pipe(
+  getOptions(): Observable<string[]> {
+    return this.httpClient.get<string[]>(`${this.baseUrl}/options`).pipe(
       catchError((error) => {
         alert('Could not get options');
         throw error;
